e8d502e2fcc37f4e7f0e70b1530bd89fec7b79c5
Will Chiong <will@infomofo.com>
2014-09-01 22:59:31 -0400

    add support for upcoming sales endpoints

--- sale.go
+++ sale.go
@@ -2,6 +2,11 @@ package go_gilt_api
 
 import "time"
 
+// From https://dev.gilt.com/documentation/data_structures.html#image_url_objects
+// Sale and product methods can both contain structures that expose image URLs. Images are provided at multiple
+// resolutions and a particular resolution can also have more than one image associated with it. An image_url object
+// consists of a set of key/value pairs with the key being the image resolution WIDTHxHEIGHT and the value being an
+// array of image objects.
 type ImageUrl struct {
 	Url    string `json:"url"`
 	Width  int    `'json:"width"`
@@ -19,4 +24,17 @@ type SaleListObject struct {
 	SaleUrl     string                `json:"sale_url"`
 	Begins      time.Time             `json:"begins"`
 	ImageUrls   map[string][]ImageUrl `json:"image_urls"`
+}
+
+// Information on a product
+// more information is available at https://dev.gilt.com/documentation/data_structures.html#product_detail_object
+type ProductDetail struct {
+}
+
+// a sale detail object extends the sale list object and provides additional fields (ends, products)
+// more information is available at https://dev.gilt.com/documentation/data_structures.html#sale_detail_object
+type SaleDetailObject struct {
+	*SaleListObject
+	Ends     time.Time `json:"ends"`
+	Products []string  `json:"products"`
 }
--- sales.go
+++ sales.go
@@ -2,6 +2,8 @@ package go_gilt_api
 
 const (
 	salesUrl = "sales"
+	activeJson = "active.json"
+	upcomingJson = "upcoming.json"
 )
 
 type salesResponse struct {
@@ -9,9 +11,35 @@ type salesResponse struct {
 }
 
 // Returns a list of active sales on gilt.com
+// for more info see: https://dev.gilt.com/documentation/resources.html#toc_163
 func (a GiltApi) GetSalesActive() (sales []SaleListObject, err error) {
 	response_ch := make(chan response)
 	activeSales := new(salesResponse)
-	a.queryQueue <- query{baseUrl + salesUrl + "/active.json", &activeSales, response_ch}
+	a.queryQueue <- query{baseUrl + salesUrl + "/" + activeJson, &activeSales, response_ch}
 	return activeSales.Sales, (<-response_ch).err
+}
+
+// Returns the list of active sales for a particular store
+func (a GiltApi) GetSalesActiveInStore(store Store) (sales []SaleListObject, err error) {
+	response_ch := make(chan response)
+	activeSales := new(salesResponse)
+	a.queryQueue <- query{baseUrl + salesUrl + "/" + string(store) + "/" + activeJson, &activeSales, response_ch}
+	return activeSales.Sales, (<-response_ch).err
+}
+
+// Returns a list of upcoming sales sales on gilt.com
+// For more info see: https://dev.gilt.com/documentation/resources.html#toc_164
+func (a GiltApi) GetSalesUpcoming() (sales []SaleListObject, err error) {
+	response_ch := make(chan response)
+	upcomingSales := new(salesResponse)
+	a.queryQueue <- query{baseUrl + salesUrl + "/" + upcomingJson, &upcomingSales, response_ch}
+	return upcomingSales.Sales, (<-response_ch).err
+}
+
+// Returns the list of upcoming sales for a particular store
+func (a GiltApi) GetSalesUpcomingInStore(store Store) (sales []SaleListObject, err error) {
+	response_ch := make(chan response)
+	upcomingSales := new(salesResponse)
+	a.queryQueue <- query{baseUrl + salesUrl + "/" + string(store) + "/" + upcomingJson, &upcomingSales, response_ch}
+	return upcomingSales.Sales, (<-response_ch).err
 }
